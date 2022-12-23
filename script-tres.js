7939d8b74c3988f0a873c1dfabf397fb3ebd9783
Carlos Efrain Garza Mendoza <carlos.erojo@live.com.mx>
2022-12-23 07:31:13 -0600

    fixes

--- script-tres.js
+++ script-tres.js
@@ -1,11 +1,11 @@
-function estimado(noches, personas, precioHab) { 
+function estimado(noches, personas, precioHab) {
     let personaExtra = 250;
     let costoExtra = 0;
     let extraQty = 0;
     let precioNoches = 0;
     precioNoches = noches * precioHab;
 
-    if (personas > 1 && personas < 5 ) {
+    if (personas > 1 && personas < 5) {
         extraQty = personas - 1;
         costoExtra = (personaExtra * extraQty) * noches;
     }
@@ -15,15 +15,15 @@ function estimado(noches, personas, precioHab) {
 }
 
 const habitaciones = [ // Esto es un array
-    { id: 1, tipo: "Plus", precio: 1250, tamano: "30m2", amenidades: "A/C, Wifi, Toiletries, WC Privado", imgUrl: "./imagenes/plus.jpg"},  // Esto es un objeto
-    { id: 2, tipo: "Estandar", precio: 1100, tamano: "25m2", amenidades: "A/C, Wifi, Toiletries, WC Privado", imgUrl: "./imagenes/estandar.jpg"},
-    { id: 3, tipo: "Micro", precio: 900, tamano: "15m2", amenidades: "A/C, Wifi, Toiletries, WC Compartido", imgUrl: "./imagenes/micro.jpg"},
-    { id: 4, tipo: "Cabina", precio: 500, tamano: "5m2", amenidades: "A/C, Wifi, WC Compartido", imgUrl: "./imagenes/cabina.jpg"}
+    { id: 1, tipo: "Plus", precio: 1250, tamano: "30m2", amenidades: "A/C, Wifi, Toiletries, WC Privado", imgUrl: "./imagenes/plus.jpg" },  // Esto es un objeto
+    { id: 2, tipo: "Estandar", precio: 1100, tamano: "25m2", amenidades: "A/C, Wifi, Toiletries, WC Privado", imgUrl: "./imagenes/estandar.jpg" },
+    { id: 3, tipo: "Micro", precio: 900, tamano: "15m2", amenidades: "A/C, Wifi, Toiletries, WC Compartido", imgUrl: "./imagenes/micro.jpg" },
+    { id: 4, tipo: "Cabina", precio: 500, tamano: "5m2", amenidades: "A/C, Wifi, WC Compartido", imgUrl: "./imagenes/cabina.jpg" }
 ]
 
 let contenedorHabitaciones = document.getElementById("contenedorHabitaciones")
 let seleccion = document.getElementById("Habitacion Seleccionada")
-let arrayHabitaciones = [ ]
+let arrayHabitaciones = []
 
 renderizarHabitaciones(habitaciones)
 
@@ -41,89 +41,98 @@ function renderizarHabitaciones(arrayHabitaciones) {
         `
 
         contenedorHabitaciones.append(tarjetaHabitacion)
+
+        let boton = document.getElementById(habitacion.id)
+        boton.addEventListener("click", tipoHabitacion)
+        boton.tipoHabitacion = habitacion.tipo;
     }
 }
-let botones = document.getElementsByClassName("botonHabitacion")
-for (const boton of botones) {
-    boton.addEventListener("click", tipoHabitacion)
-}
 
-let precioHabitacion 
-function tipoHabitacion() {
-  precioHabitacion = habitaciones.find(habitacion => habitacion.tipo === tipoHabitacion).precio
+//let botones = document.getElementsByClassName("botonHabitacion")
+//for (const boton of botones) {
+//    boton.addEventListener("click", tipoHabitacion)
+//}
 
-  if (tipoHabitacion) {
-    Swal.fire(qtyNoches)
-  }
+let precioHabitacion = 0;
+function tipoHabitacion(event) {
+    precioHabitacion = habitaciones.find(habitacion => habitacion.tipo === event.currentTarget.tipoHabitacion).precio
+
+    if (tipoHabitacion) {
+        qtyNoches();
+    }
 }
 
 // -------------------------------
-
+let cantidadNoches = 0;
 async function qtyNoches() { // Cuantas noches
     const ipAPI = '//api.ipify.org?format=json'
 
     const inputValue = fetch(ipAPI)
-    .then(response => response.json())
-    .then(data => data.ip)
+        .then(response => response.json())
+        .then(data => data.ip)
 
     const { value: quantityNights } = await Swal.fire({
-    title: '¿Cuántas noches te gustaría obtener un estimado?',
-    input: 'text',
-    inputValue: inputValue,
-    showCancelButton: true,
-    inputValidator: (value) => {
-        if (!value) {
-        return 'Necesitas escribir algo!'
+        title: '¿Cuántas noches te gustaría obtener un estimado?',
+        input: 'text',
+        inputValue: inputValue,
+        showCancelButton: true,
+        inputValidator: (value) => {
+            if (!value) {
+                return 'Necesitas escribir algo!'
+            }
         }
-    }
     })
 
     if (quantityNights) {
-    Swal.fire(qtyPersons)
+        cantidadNoches = quantityNights;
+        qtyPersonas();
     }
 }
 
+let cantidadPersonas = 0;
 async function qtyPersonas() { // Cuantas noches
-  const ipAPI = '//api.ipify.org?format=json'
+    const ipAPI = '//api.ipify.org?format=json'
 
-  const inputValue = fetch(ipAPI)
-  .then(response => response.json())
-  .then(data => data.ip)
+    const inputValue = fetch(ipAPI)
+        .then(response => response.json())
+        .then(data => data.ip)
 
-  const { value: quantityPersons } = await Swal.fire({
-  title: '¿Cuantás personas se hospedan en la misma habitación?',
-  input: 'text',
-  inputValue: inputValue,
-  showCancelButton: true,
-  inputValidator: (value) => {
-      if (!value) {
-      return 'Necesitas escribir algo!'
-      }
-  }
-  })
+    const { value: quantityPersons } = await Swal.fire({
+        title: '¿Cuantás personas se hospedan en la misma habitación?',
+        input: 'text',
+        inputValue: inputValue,
+        showCancelButton: true,
+        inputValidator: (value) => {
+            if (!value) {
+                return 'Necesitas escribir algo!'
+            }
+        }
+    })
 
-  if (quantityPersons) {
-  Swal.fire(totalEstimado)
-  }
+    if (quantityPersons) {
+        cantidadPersonas = quantityPersons;
+        totalEstimado();
+    }
 }
 
 function totalEstimado() { // resultado final de la cotizacion
+    let precioEstimado = estimado(cantidadNoches, cantidadPersonas, precioHabitacion);
+
     Swal.fire({
         title: 'Tu precio estimado es de:',
-        text: '$ ' + precioEstimado + 'MXN' ,
+        text: '$ ' + precioEstimado + 'MXN',
         icon: 'success',
         confirmButtonText: 'Cool'
-      })
-    
+    })
+
 }
 
 // ------------------------------------
- 
-let precioEstimado = estimado(qtyNoches, qtyPersonas, precioHabitacion);
+
 
 
 /*
-- Crear tarjetas, que se pueda hacer la seleccion de una y que con eso se despliegue el primer sweet alert 
+- Crear tarjetas, que se pueda hacer la seleccion de una y que con eso se despliegue el primer sweet alert
 - el primer swal debe preguntar cuantas noches te quieres quedar, el boton debe decir siguiente
 - segundo swal debe preguntar cuantas personas se hospedan en la misma habitacion, el boton debe decir ok
 - el ultimo swal debe mostrar el precio total estimado
